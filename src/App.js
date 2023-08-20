684aa62b9389a63d796ac6c56548a04b2b56e8d0
cosjiimos <jiin4900@gmail.com>
2023-08-20 17:33:42 +0900

    jiin_start_button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import Album from "./Tutorial.js";
+import T from "./Tutorial.js";
+import S from "./CMLtest_Simulation.js"
+import N from "./CMLtest_NoSimulation.js"
 //    CMLtest_NoSimulation   CMLtest_NoSimulation     Tutorial
-import { createTheme } from '@mui/material/styles'; 
-import { useEffect } from 'react';
-import React, { useState } from 'react'; 
+import React, { useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 import axios from 'axios';
+import CssBaseline from '@mui/material/CssBaseline';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import TextField from '@mui/material/TextField';
 
 
 axios.defaults.baseURL = "http://166.104.34.158:5010";
@@ -16,13 +25,53 @@ const darkTheme = createTheme({
   },
 });
 
-
-export default function App () {
-
- //버튼&슬라이더 변경! 
- return (
-  <div className="App">
-    <Album/>
-  </div>
-);
+export default function App() {
+  const [showAlbum, setShowAlbum] = useState(false);
+  const [p_no, setP_no] = useState('');
+  const [system, setSystem] = useState('');
+  async function exp_info() {
+    if (p_no && (system === 'simul' || system === 'nosimul' || system === 'tutorial')) {
+      try {
+        const response = await axios.post('/exp_info', {
+          p_no:p_no,
+          system:system
+        });
+        setShowAlbum(true);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+  
+  return (
+    <div className="App">
+      {!showAlbum && (
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <Container sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '100vh'}} maxWidth="false">
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="system-label">System</InputLabel>
+                    <Select
+                        labelId="system-label"
+                        id="system-select"
+                        value={system}
+                        onChange={(e) => setSystem(e.target.value)}
+                    >
+                        <MenuItem value="S">S</MenuItem>
+                        <MenuItem value="N">N</MenuItem>
+                        <MenuItem value="T">T</MenuItem> 
+                    </Select>
+                </FormControl>
+                <TextField label="p_no" variant="filled" value={p_no} onChange={(e) => setP_no(e.target.value)} sx={{ m: 1 }} />
+            </div>
+            <Button sx={{ mt: 2, width:'10%'}} disabled={!p_no || !system} onClick={() => exp_info()} variant="contained" color="secondary">Start</Button>
+        </Container>
+        </ThemeProvider>
+      )}
+      {showAlbum && system === "S" && <simul />}
+      {showAlbum && system === "N" && <nosimul />}
+      {showAlbum && system === "Y" && <tutorial />}
+    </div>
+  );
 }
